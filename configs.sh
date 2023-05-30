abbc64ce1ff52f43ebfd2263ba7317f02ce5021b
hand32 <hand32@harness.hand32-158608.bayopsys-pg0.utah.cloudlab.us>
2023-05-30 16:44:28 -0600

    set up directories

--- configs.sh
+++ configs.sh
@@ -1,9 +1,9 @@
 # Set this to point to the top level of the TailBench data directory
-DATA_ROOT=/topleveldatadir
+DATA_ROOT=/data/tailbench.inputs
 
 # Set this to point to the top level installation directory of the Java
 # Development Kit. Only needed for Specjbb
  JDK_PATH=/pathtojdk
 
 # This location is used by applications to store scratch data during execution.
-SCRATCH_DIR=/pathtoscratch
+SCRATCH_DIR=/users/hand32/tailbench-v0.9/scratch
