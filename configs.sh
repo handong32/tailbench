20c6b16e17ed4de3328790cde38222214bb6aa20
hand32 <hand32@harness.hand32-158608.bayopsys-pg0.utah.cloudlab.us>
2023-05-30 16:51:34 -0600

    update JDK path

--- configs.sh
+++ configs.sh
@@ -3,7 +3,7 @@ DATA_ROOT=/data/tailbench.inputs
 
 # Set this to point to the top level installation directory of the Java
 # Development Kit. Only needed for Specjbb
- JDK_PATH=/pathtojdk
+ JDK_PATH=/usr/lib/jvm/java-8-openjdk-amd64
 
 # This location is used by applications to store scratch data during execution.
 SCRATCH_DIR=/users/hand32/tailbench-v0.9/scratch
