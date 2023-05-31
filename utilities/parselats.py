006fc815a07ae31b8518d65026ad278096091b42
hand32 <hand32@harness.hand32-158637.bayopsys-pg0.utah.cloudlab.us>
2023-05-31 13:37:25 -0600

    fixes to work with python3 and additional latency outputs

--- utilities/parselats.py
+++ utilities/parselats.py
@@ -9,7 +9,7 @@ class Lat(object):
     def __init__(self, fileName):
         f = open(fileName, 'rb')
         a = np.fromfile(f, dtype=np.uint64)
-        self.reqTimes = a.reshape((a.shape[0]/3, 3))
+        self.reqTimes = a.reshape((int(a.shape[0]/3), 3))
         f.close()
 
     def parseQueueTimes(self):
@@ -39,10 +39,17 @@ if __name__ == '__main__':
             f.write("%12s | %12s | %12s\n" \
                     % ('%.3f' % q, '%.3f' % svc, '%.3f' % sjrn))
         f.close()
+        p50 = stats.scoreatpercentile(sjrnTimes, 50)
+        p75 = stats.scoreatpercentile(sjrnTimes, 75)
+        p90 = stats.scoreatpercentile(sjrnTimes, 90)
         p95 = stats.scoreatpercentile(sjrnTimes, 95)
+        p99 = stats.scoreatpercentile(sjrnTimes, 99)
         maxLat = max(sjrnTimes)
-        print "95th percentile latency %.3f ms | max latency %.3f ms" \
-                % (p95, maxLat)
+        print("50th percentile latency %.3f ms | max latency %.3f ms" % (p50, maxLat))
+        print("75th percentile latency %.3f ms | max latency %.3f ms" % (p75, maxLat))
+        print("90th percentile latency %.3f ms | max latency %.3f ms" % (p90, maxLat))
+        print("95th percentile latency %.3f ms | max latency %.3f ms" % (p95, maxLat))
+        print("99th percentile latency %.3f ms | max latency %.3f ms" % (p99, maxLat))
 
     latsFile = sys.argv[1]
     getLatPct(latsFile)
