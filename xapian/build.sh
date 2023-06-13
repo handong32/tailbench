65de68969bf82985ac668cfd90d572113a982f1f
hand32 <hand32@server.hand32-159215.bayopsys-pg0.utah.cloudlab.us>
2023-06-13 15:07:35 -0600

    ld path and xapian build script modify

--- xapian/build.sh
+++ xapian/build.sh
@@ -1,16 +1,13 @@
 #/bin/bash
 
 # Install Xapian core if not already present
-if [[ ! -d xapian-core-1.2.13 ]]
-then
-    tar -xf xapian-core-1.2.13.tar.gz
-    cd xapian-core-1.2.13
-    mkdir install
-    ./configure --prefix=$PWD/install
-    make -j16
-    make install
-    cd -
-fi
+
+cd xapian-core-1.2.13
+mkdir install
+./configure --prefix=$PWD/install
+make -j16
+make install
+cd -
 
 # Build search engine
 make
